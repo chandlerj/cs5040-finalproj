7674279f6a89397c838dc0e47d7c2199cefa310b
emilylabonty <157996126+emilylabonty@users.noreply.github.com>
2025-04-10 10:03:43 -0600

    Update hardcoded dir path in .psvm file

---
+++ dir_change.py
@@ -0,0 +1,30 @@
+
+import sys
+
+# Replaces instances of 'old_dir' with 'new_dir' in a .pvsm file.
+def replace_directory_in_pvsm(pvsm_file, old_dir, new_dir):
+    """
+    Args:
+        pvsm_file (str): Path to .pvsm file
+        old_dir (str): Original directory name
+        new_dir (str): Desired directory name
+
+        EXAMPLE: python "isostate.psvm" "/home/chandler/Documents/cs6040-finalproj/run01-0.44/run" "/run01/run01/timestep"
+    """
+    try:
+        with open(pvsm_file, 'r') as file:
+            content = file.read()
+    except FileNotFoundError:
+        print(f"Error: File not found: {pvsm_file}")
+        return
+
+    new_content = content.replace(old_dir, new_dir)
+
+    try:
+        with open(pvsm_file, 'w') as file:
+            file.write(new_content)
+        print(f"Success")
+    except Exception as e:
+        print(f"Error occurred while writing to: {e}")
+
+
